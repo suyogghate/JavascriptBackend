df41b9779dbac19ba8576313296b435472cd442d
suyog <suyogghate96@gmail.com>
2024-07-22 18:14:24 +0530

    fullstack project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import './App.css'
+
+function App() {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/jokes")
+      .then((reponse) => {
+        setJokes(reponse.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  })
+  
+  return (
+    <>
+      <h1>Chai aur Fullstack</h1>
+      <p>JOKES: {jokes.length}</p>
+
+      {
+        jokes.map((joke, index) => (
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        ))
+      }
+    </>
+  )
+}
+
+export default App
